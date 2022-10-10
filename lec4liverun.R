05dbb81f5a6d05e23f33a68bb764d1d53965e747
Instructor Winegarden <instructor@Thomass-MacBook-Pro-2.local>
2022-10-10 11:52:45 -0700

    quadratic formula fn debugging

--- lec4liverun.R
+++ lec4liverun.R
@@ -56,3 +56,78 @@ my_contradiction1 <- contradict("I'm cool")
 my_contradiction2 <- contradict("I'm not so bad instructing")
 print(my_contradiction2)
 
+############### PART 2 ##########
+#Debugging
+
+text <- "hello sup"
+
+#a <- 1+ 1
+# 
+#"val <- min(1, 3
+
+fn_1 <- function(a) {
+  result <- a * 2
+  return(result)
+}
+
+#general quadratic equation: ax^2 + bx + c = 0
+#solve for x
+#x = -b plus or minus the square root of b^2 - 4ac
+# all over 2a
+
+quadratic_formula <- function(a, b, c) {
+  temp1 <- -b / (2 * a)
+  temp2 <- sqrt(b^2 - 4 * a * c)
+  val1 <- temp1 + temp2
+  val2 <- temp1 - temp2
+  return(answer)
+}
+
+quadratic_formula(1, 0, 1)
+
+quadratic_formula <- function(a, b, c) {
+  
+  a <- 1
+  b <- 2
+  c <- 1
+  print(paste("start of function", a, b, c))
+  temp1 <- -b / (2 * a)
+  print(paste("temp1 =", temp1))
+  
+  variable_to_squareroot <- b^2 - 4 * a * c
+  sqrt(-4)
+  sqrt(variable_to_squareroot)
+  
+  temp2 <- sqrt(b^2 - 4 * a * c)
+  print(paste("temp2 =", temp2))
+  
+  print("start +/- part here")
+  val1 <- temp1 + temp2
+  val2 <- temp1 - temp2
+  
+  print("combine answers into a vector")
+  answer <- c(val1, val2)
+  return(answer)
+}
+
+quadratic_formula <- function(a, b, c) {
+  
+  print(paste("start of function", a, b, c))
+  temp1 <- -b / (2 * a)
+  print(paste("temp1 =", temp1))
+  
+  temp2 <- sqrt(b^2 - 4 * a * c)
+  print(paste("temp2 =", temp2))
+  
+  print("start +/- part here")
+  val1 <- temp1 + temp2
+  val2 <- temp1 - temp2
+  
+  print("combine answers into a vector")
+  answer <- c(val1, val2)
+  return(answer)
+}
+
+quadratic_formula(1, 4, 2)
+
+
