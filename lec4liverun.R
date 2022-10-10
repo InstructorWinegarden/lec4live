afad16a609b60e1d9f950917e95967d0763abf38
Instructor Winegarden <instructor@Thomass-MacBook-Pro-2.local>
2022-10-10 10:59:06 -0700

    circumference function

--- lec4liverun.R
+++ lec4liverun.R
@@ -1,3 +1,27 @@
 #lecture 4 live demo code for those who want to copy/reference
 
 min(1, 4, 0)
+
+add_numbers <- function(a, b) {
+  answer = a + b
+  return(answer)
+}
+
+add_numbers(20, 7)
+
+num_added <- add_numbers(20, 7)
+num_added <- add_numbers(25, 7)
+num_added <- add_numbers(num_added, 11)
+
+
+print(num_added)
+
+#find circumference of circle based on the diameter
+circumference_from_diameter <- function(diameter) {
+  circumference <- diameter * pi
+  return(circumference)
+}
+
+circumference_from_diameter(8)
+
+my_circumference <- circumference_from_diameter(8)
